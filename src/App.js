17d4c49fe7a6404a4ead7d6c766c74c0f6e4ea58
ElLettuce <81658867+ElLettuce@users.noreply.github.com>
2022-04-30 13:51:28 -0500

    fixed issues with mobile view

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import "./App.css"
-import React, { Component } from "react"
+import React, {Component} from "react"
 import Header from "./components/Header"
 import AssetMenu from "./components/AssetMenu/AssetMenu"
 import AssetDetails from "./components/AssetDetails"
@@ -13,7 +13,8 @@ export default class App extends Component {
         this.state = {
             selectedCategory: null,
             selectedAssetKey: null,
-            messages: []
+            messages: [],
+            width: window.innerWidth
         }
         window.app = this
         this.removeMessage = this.removeMessage.bind(this)
@@ -39,27 +40,47 @@ export default class App extends Component {
         })
     }
 
+    componentDidMount() {
+        window.addEventListener('resize', this.handleResize.bind(this))
+    }
+
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.handleResize.bind(this))
+    }
+
+    handleResize(){
+        this.setState({width: window.innerWidth})
+    }
+
+    popupContent(close){
+        return <div className="modal">
+            <AssetMenu selectAsset={this.selectAsset.bind(this)}/>
+            <div className="close" onClick={close}>&times;</div>
+        </div>
+    }
+
     render() {
+        const isSmallScreen = this.state.width <= 812 // 812px is defined in App.css
         return (
             <div>
                 <Header />
-                <div class ="mobileAssetsContainer">
-                    <Popup
-                    trigger={<button class="chooseAssetButton"  >ASSETS</button>}
-                    modal={true}
-                    closeOnEscape={false}
-                    closeOnDocumentClick={true}                    
-                    >
-                    <AssetMenu  selectAsset={this.selectAsset.bind(this)} />
-                    {close => this.popupContent(close)}
-                    </Popup>
-
-                    <AssetDetails
-                    selectedCategory={this.state.selectedCategory}
-                    selectedAssetKey={this.state.selectedAssetKey}
-                    />
-                </div>
-                <div class ="assetsContainer">
+                {isSmallScreen && <div className="mobileAssetsContainer">
+                        <Popup
+                            trigger={<button className="chooseAssetButton">ASSETS</button>}
+                            modal={true}
+                            contentStyle={{backgroundColor: "rgb(0,0,0,0)", border: "none"}}
+                            closeOnEscape={false}
+                            closeOnDocumentClick={false}
+                        >
+                            {close => this.popupContent(close)}
+                        </Popup>
+                        <AssetDetails
+                            selectedCategory={this.state.selectedCategory}
+                            selectedAssetKey={this.state.selectedAssetKey}
+                        />
+                    </div>
+                }
+                <div className="assetsContainer">
                     <AssetDetails
                         selectedCategory={this.state.selectedCategory}
                         selectedAssetKey={this.state.selectedAssetKey}
