70f6075d2322352f9c4dd753af34dc1dda3de64a
tallow-io <111828227+tallow-io@users.noreply.github.com>
2022-11-11 11:41:12 -0600

    Update URL.js

--- src/URL.js
+++ src/URL.js
@@ -1,1 +1,1 @@
-export const URL = 'Insert URL here' //Paste the URL from the terminal, keep the quotes. Click the pencil button to edit this file.
+export const URL = 'https://ciobrain-api-aihtesting0000003.azurewebsites.net/asset' //Paste the URL from the terminal, keep the quotes. Click the pencil button to edit this file.
